775378162572ecf8ea18c372a6ba51550c2a9bbb
Adan2GG <167446338+Adan2GG@users.noreply.github.com>
2024-07-05 12:34:45 +0200

    Update vars.tf. Add vars vm, kubernete

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -21,3 +21,51 @@ variable "sku" {
   type        = string
   default     = "Standard"
 }
+
+variable "network_name" {
+  description = "The name of the Azure network"
+  type        = string
+  default     = "networkagg"
+}
+
+variable "subnet_name" {
+  description = "The name of the Azure subnet"
+  type        = string
+  default     = "subnetagg"
+}
+
+variable "nic_name" {
+  description = "The name of the Azure intefaz net"
+  type        = string
+  default     = "nicagg"
+}
+
+variable "vm_name" {
+  description = "The name of the Azure Virtual Machine"
+  type        = string
+  default     = "vmUbuntuagg"
+}
+
+variable "adminuser" {
+  description = "The value of adminuser of VM Ubuntu"
+  type        = string
+  default     = "ubuntu"
+}
+
+variable "computer_name" {
+  description = "The name of VM"
+  type        = string
+  default     = "ubuntuagg"
+}
+
+variable "admin_pass" {
+  description = "The value of pss admin of VM"
+  type        = string
+  default     = "ubuntuagg"
+}
+
+variable "clusteraks_name" {
+  description = "The value of pss admin of VM"
+  type        = string
+  default     = "akagg"
+}
