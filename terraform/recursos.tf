28c02df47e19aa71a1e99e9d7325524c698d0247
Adan2GG <167446338+Adan2GG@users.noreply.github.com>
2024-07-17 17:22:21 +0200

    Update recursos.tf

--- terraform/recursos.tf
+++ terraform/recursos.tf
@@ -24,7 +24,7 @@ resource "azurerm_linux_virtual_machine" "mvlinux" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "22.04-LTS"
     version   = "latest"
   }
 
